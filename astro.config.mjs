5e080bde8e1ee7be517712d158d5d790c889dd53
CarlDesigner <krlitosfajardo626@gmail.com>
2024-09-08 18:56:31 -0500

    🐛 [FIX] Bug deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import { defineConfig } from 'vite';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';

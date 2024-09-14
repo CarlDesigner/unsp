b104d5c4180c38dfe9efb19c88e6fe3d8822e3a4
CarlDesigner <krlitosfajardo626@gmail.com>
2024-09-14 18:11:48 -0500

    💩 [ADD] Script reload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,43 +1,37 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import react from '@astrojs/react'
-import { remarkReadingTime } from './src/utils/readTime.ts'
-import icon from 'astro-icon'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import { remarkReadingTime } from './src/utils/readTime.ts';
+import icon from 'astro-icon';
+
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://unsp.vercel.app/',
-	// Write here your website url
-	markdown: {
-		remarkPlugins: [remarkReadingTime],
-		drafts: true,
-		shikiConfig: {
-			theme: 'one-dark-pro',
-			wrap: true
-		}
-	},
-	integrations: [
-		mdx({
-			syntaxHighlight: 'shiki',
-			shikiConfig: {
-				experimentalThemes: {
-					light: 'vitesse-light',
-					dark: 'one-dark-pro'
-				},
-				wrap: true
-			},
-			drafts: true
-		}),
-		sitemap(),
-		tailwind(),
-		icon(),
-		react()
-	],
-
-	// Desactiva la barra de herramientas de Astro DevTools
-	devToolbar: {
-		enabled: false
-	},
-})
+  site: 'https://unsp.vercel.app/',
+  // Write here your website url
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+    drafts: true,
+    shikiConfig: {
+      theme: 'one-dark-pro',
+      wrap: true
+    }
+  },
+  integrations: [mdx({
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      experimentalThemes: {
+        light: 'vitesse-light',
+        dark: 'one-dark-pro'
+      },
+      wrap: true
+    },
+    drafts: true
+  }), sitemap(), tailwind(), icon(), react()],
+  // Desactiva la barra de herramientas de Astro DevTools
+  devToolbar: {
+    enabled: false
+  }
+});

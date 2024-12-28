b931c87e5a6d4541b8c0df61f3076673bfc50cfd
CarlDesigner <krlitosfajardo626@gmail.com>
2024-12-27 21:16:43 -0500

    🍱 [UPDATE] Articles category

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,7 +1,7 @@
 // List of categories for blog posts
 export const CATEGORIES = [
 	'Programación',
-	'Tips y Trucos',
+	/* 'Tips y Trucos',
 	'Recursos',
-	'Noticias y Tendencias',
+	'Noticias y Tendencias', */
 ] as const

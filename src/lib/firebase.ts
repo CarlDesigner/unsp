5572d8ee2c775c4fa429c7f0a37c6f839fa3bf90
CarlDesigner <krlitosfajardo626@gmail.com>
2025-04-21 12:08:29 -0500

    🔥 [REMOVE] .env

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import dotenv from 'dotenv';
-dotenv.config();
 
 // Configuración de Firebase
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: "AIzaSyB55XG7fEiEH4OmCViMRcsUQHjXRUVRMq0",
+  authDomain: "cokkiedatabase.firebaseapp.com",
+  projectId: "cokkiedatabase",
+  storageBucket: "cokkiedatabase.firebasestorage.app",
+  messagingSenderId: "976221604755",
+  appId: "1:976221604755:web:c85f9811cb7432c1bff666"
 };
 
 // Inicializar Firebase
